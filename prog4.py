14e7ae34102824a6e535049552dddda7ae7a33d7
272002 <108273339+272002@users.noreply.github.com>
2022-07-15 14:50:34 -0700

    commit

---
+++ prog4.py
@@ -0,0 +1,6 @@
+print(" program to calculate simple interest ")
+P = int(input("Enter principal amount "))
+R = float(input(" Enter the rate of interest "))
+T = int(input("Enter time in years "))
+SI = int(P*R*T)/100;
+print(SI)
