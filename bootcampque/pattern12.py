240fcd3bd401db95f4f5f1c0dcd09ac945a18203
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-10-01 09:55:23 +0530

    commit

---
+++ bootcampque/pattern12.py
@@ -0,0 +1,13 @@
+k = int(9/2)
+for outer in range(0,9):
+    if outer < 5:
+        for inner in range(0,outer+1):
+            print("*",end='')
+        print("\n")
+    else:
+        for inner in range(0,k):
+            print("*",end='')
+        k = k-1
+        print("\n")
+
+
