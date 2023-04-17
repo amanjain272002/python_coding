0a06c0032d0ba9d13faf5f306b105b89254eab9f
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-04-17 21:38:03 +0530

    commit

---
+++ bigdata/17April.py
@@ -0,0 +1,14 @@
+# age  = 100
+# print(age)
+# print(type(age))
+
+# name = 'aman'
+# print(name,end=" ")
+# print("Hello")
+# print(type(name))
+
+
+# multi line string
+# s = """b name   
+# hello"""
+# print(s)
