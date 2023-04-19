03a771eeb22f90f1501ec5de965c834db5ff261a
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-04-19 22:03:38 +0530

    commit

---
+++ bigdata/18April.py
@@ -0,0 +1,30 @@
+# x = "abc"
+# y = "Abc"
+# print(id(x))
+# print(id(y))
+
+# a= x;
+# print(id(a))
+# print(id(x))
+
+# y = "qrt"
+# del y
+# print(y)
+
+# fname = "hello"
+# lname = "world"
+# print("The text is",fname,lname)
+# print("The first value is ",fname,"last name is ",lname)
+# print(f"The first value is {fname}  last name is {lname}")
+
+# no change when space is added
+# print(f"The first value is {fname       }  last name is {lname}")
+# print(f"The first value is {fname}  last name is {lname1}") 
+
+# string formatting
+# username = "aman"
+# print("hey user {}".format("hello"))
+# print("hey user {}".format(username))
+
+# operators
+# print(12.34//4)
