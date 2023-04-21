89e42a8bdbd438184e6280b25a033d1e61d8af11
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-04-21 21:26:19 +0530

    commit

---
+++ bigdata/20April.py
@@ -0,0 +1,13 @@
+# x = 10
+# if type(x) is int:
+#     print("hello")
+
+# x = 100
+# y = 100
+# print(id(x)," ",id(y))
+# x = 400
+# y = 400
+# print(id(x)," ",id(y))
+
+
+# print(bin(4))
---
+++ bigdata/21April.py
@@ -0,0 +1,43 @@
+# ===>  que 1
+
+# x = 27
+# if (x>20):
+#     print("greater than 20")
+# if (x>25):
+#     if (x>25 and x<30):
+#         print("greater than 25 and less than 30")
+#     else:
+#         print("greater than 25")
+# else:
+#     print("less than 20")
+
+# ===>  que 2
+# stri = input()
+# for i in stri:
+#     if(i == "a" or i == "e" or i == "o" or i == "u"):
+#         print(i)
+
+# ===>  que 3
+# stri = input()
+# l = len(stri)
+# if ((stri[0] == "a" or stri[0] == "e" or stri[0] == "o" or stri[0] == "u") or (stri[l-1] == "a" or stri[l-1]  == "e" or stri[l-1]  == "o" or stri[l-1]  == "u")):
+#     print("yes")
+# else:
+#     print("no")
+
+
+# ===>  que 4
+# num = int(input())
+# for i in range(2,11):
+#     if (num%i==0):
+#         print(i)
+
+
+# ===>  que 5
+# for outer in range(1,6):
+#     for inner in range(0,6-outer):
+#         print("*",end=" ")
+
+#     print()    
+
+
