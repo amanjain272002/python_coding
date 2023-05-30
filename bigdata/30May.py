c6ead377a8b780759560053d7f8ce5a3029e4522
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-05-30 15:44:07 +0530

    commit

---
+++ bigdata/30May.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# ar1 = np.array([[1,2,3],[4,5,6]])
+# ar2 = np.array([10,20,344])
+# print(ar1+ar2)
+
+
+ar3 = np.array([[1,3],[4,5],[6,8]])
+ar4 = np.array([[10],[20],[344]])
+print(ar3+ar4)
