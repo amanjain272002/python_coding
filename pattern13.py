be1379553319abad1b48110b01864e59e6f1c31d
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-12-05 09:40:30 +0530

    commit

---
+++ pattern13.py
@@ -0,0 +1,15 @@
+for outer in range(1,6):
+    for inner in range(1,6):
+        if(inner<=5-outer):
+            print(" ",end='')
+        else:
+            print("*",end='')
+    print()
+
+for outer in range(4,0,-1):
+    for inner in range(1,6):
+        if(inner<=5-outer):
+            print(" ",end='')
+        else:
+            print("*",end='')
+    print()
