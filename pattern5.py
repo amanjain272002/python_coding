5f977825e630e8da2f5c81dc431b574da15e4e4d
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-12-05 12:45:12 +0530

    commit

---
+++ pattern5.py
@@ -0,0 +1,11 @@
+ans = 1
+save = 0
+counter = 1
+for outer in range(1,9):
+    save = save + counter
+    ans = save
+    for inner in range(1,outer + 1):
+        print(ans," ",end='')
+        ans = ans - 1
+    print()
+    counter = counter + 1 
